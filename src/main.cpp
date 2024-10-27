6bbfb880bd7e7fdd8939389db5c371ecb9f71caf
TheHuntsmanbuh <158553946+TheHuntsmanbuh@users.noreply.github.com>
2024-10-28 10:41:45 +1100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,121 @@
+#include<iostream>
+#include<string>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<cmath>
+#include<stb/stb_image.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+
+
+#include"camera.h"
+#include"Model.h"
+
+using namespace std;
+
+const string version = "Tuesday Engine ALPHA 0.0.1";
+const unsigned int width = 1920;
+const unsigned int height = 1080;
+
+
+
+
+
+
+int main()
+{
+	// Initialize GLFW
+	glfwInit();
+	
+
+
+	// Tell GLFW what version of OpenGL we are using 
+	// In this case we are using OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Tell GLFW we are using the CORE profile
+	// So that means we only have the modern functions
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+   
+
+	// WINDOW CREATION PARAMS 
+	GLFWwindow* window = glfwCreateWindow(width, height, "Tuesday Engine ALPHA 0.0.1", NULL, NULL);
+	// Error check if the window fails to create
+	if (window == NULL)
+	{
+		std::cout << "ERROR: tuesday aborted, Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	// Introduce the window into the current context
+	glfwMakeContextCurrent(window);
+
+
+
+	//Load GLAD so it configures OpenGL
+	gladLoadGL();
+	// Specify the viewport of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+	glViewport(0, 0, width, height);
+
+    Shader shaderProgram("src/shaders/default.vert", "src/shaders/default.frag");
+	
+	// Take care of all the light related things
+	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
+	glm::mat4 lightModel = glm::mat4(1.0f);
+	lightModel = glm::translate(lightModel, lightPos);
+
+	shaderProgram.Activate();
+	glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+	glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+	
+
+
+
+
+
+	
+	
+
+	glEnable(GL_DEPTH_TEST);
+
+
+	// creates the camera object
+	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
+    Model model("src/resources/tmdl/map/scene.gltf");
+	// Main while loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Specify the color of the background
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		// Clean the back buffer and depth buffer
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Handles camera inputs
+		camera.Inputs(window);
+		// Updates and exports the camera matrix to the Vertex Shader
+		camera.updateMatrix(45.0f, 0.1f, 100.0f);
+
+		// Draw a model
+		model.Draw(shaderProgram, camera);
+
+		// Swap the back buffer with the front buffer
+		glfwSwapBuffers(window);
+		// Take care of all GLFW events
+		glfwPollEvents();
+	}
+    // delete all the objects that have been created
+    
+	shaderProgram.Delete();
+	
+
+
+	// Delete window before ending the program
+	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
+	glfwTerminate();
+	return 0;
+}
